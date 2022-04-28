1a3d33d5ab612c01abff59072bd83c2109d23f03
misha <meja1@inbox.ru>
2022-04-28 14:51:49 +0300

    Templates

--- Site/apps.py
+++ Site/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ScythgamesConfig(AppConfig):
+class SiteConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Scythgames'
+    name = 'Site'
--- Site/views.py
+++ Site/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request, 'Site/home.html', {})
+    
+def library(request):
+    return render(request, 'Site/library.html', {})
+
+def about(request):
+    return render(request, 'Site/about.html', {})
